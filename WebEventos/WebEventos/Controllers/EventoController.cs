3b6750b8ecbe080d228e3e99c94bbaa25da64093
Abel Palomino <abel_17_87@hotmail.com>
2019-06-25 01:22:00 -0500

    Avance Eventos

--- WebEventos/WebEventos/Controllers/EventoController.cs
+++ WebEventos/WebEventos/Controllers/EventoController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebEventos.SRefEvento;
+using WebEventos.SRefAreaTematica;
 
 namespace WebEventos.Controllers
 {
     public class EventoController : Controller
     {
+        EventoServiceClient clientEv = new EventoServiceClient();
+        AreaTematicaServiceClient clientAt = new AreaTematicaServiceClient();
+
         // GET: Evento
         public ActionResult Index()
         {
-            return View();
+            List<Evento> listEvento = new List<Evento>();
+
+            try
+            {
+                listEvento = clientEv.listar().ToList();
+
+            }
+            catch (Exception ev)
+            {
+
+                //throw;
+            }
+
+            return View(listEvento);
         }
 
         // GET: Evento/Details/5
@@ -23,45 +41,103 @@ namespace WebEventos.Controllers
         // GET: Evento/Create
         public ActionResult Create()
         {
-            return View();
+            try
+            {
+
+                ViewBag.areaTematicas = comboBoxAreaTematica();
+
+                return View(new Evento());
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+                //throw;
+            }
+
+
         }
+
+
 
         // POST: Evento/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Evento evento)
         {
             try
             {
-                // TODO: Add insert logic here
+                evento.CodUsuario = 1;
+                evento.LugaresDisponibles = evento.LimiteParticipantes;
+                SRefEvento.ServiceResponse response = clientEv.registrar(evento);
 
-                return RedirectToAction("Index");
+                if (response.IsSuccess)
+                {
+                    return RedirectToAction("Index");
+
+                }
+
+                ViewBag.areaTematicas = comboBoxAreaTematica();
+
+                ViewBag.Message = response.Message;
+
+                return View(evento);
+
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                return View(evento);
             }
         }
 
         // GET: Evento/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            try
+            {
+                Evento evento = clientEv.buscar(id);
+
+                if (evento == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.areaTematicas = comboBoxAreaTematica();
+                return View(evento);
+            }
+            catch (Exception ex)
+            {
+
+                return RedirectToAction("Index");
+                //throw;
+            }
+
         }
 
         // POST: Evento/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, Evento evento)
         {
             try
             {
-                // TODO: Add update logic here
+                evento.CodEvento = id;
+                SRefEvento.ServiceResponse response = clientEv.actualizar(evento);
 
-                return RedirectToAction("Index");
+                if (response.IsSuccess)
+                {
+                    return RedirectToAction("Index");
+
+                }
+
+
+                ViewBag.areaTematicas = comboBoxAreaTematica();
+
+                ViewBag.Message = response.Message;
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
             }
+
+            return View(evento);
+
         }
 
         // GET: Evento/Delete/5
@@ -84,6 +160,11 @@ namespace WebEventos.Controllers
             {
                 return View();
             }
+        }
+
+        private IEnumerable<SelectListItem> comboBoxAreaTematica()
+        {
+            return (from at in clientAt.listar() select new SelectListItem() { Value = at.CodAreaTematica.ToString(), Text = at.NombreAreaTematica });
         }
     }
 }
