20052787aea981fa51856d395afe2442cdeafe08
Abel Palomino <abel_17_87@hotmail.com>
2019-06-30 01:51:30 -0500

    Avance control

---
+++ WebEventos/WebEventos/Models/EventoParticipanteModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebEventos.SRefParticipante;
+using WebEventos.SRefEvento;
+using WebEventos.SRefEventoParticipante;
+
+namespace WebEventos.Models
+{
+    public class EventoParticipanteModel
+    {
+        public Participante Participante { get; set; }
+        public Evento Evento { get; set; }
+        public EventoParticipante EventoParticipante { get; set; }
+        public List<Participante> Participantes { get; set; }
+        public List<Evento> Eventos { get; set; }
+    }
+}
